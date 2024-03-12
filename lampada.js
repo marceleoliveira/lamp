2c2c36119b86e5d85e360a2be530f9ec4c9890c4
Marcele Oliveira <marcele.dev@gmail.com>
2024-03-12 14:00:32 -0300

    Add files via upload

---
+++ lampada.js
@@ -0,0 +1,29 @@
+const turOn = document.getElementById("turnOn");
+const turOff = document.getElementById("turnOff");
+const lamp = document.getElementById("lamp");
+
+function isLampBroken() {
+  return lamp.src.indexOf("quebrada") > -1;
+}
+
+function lampOn() {
+  if (!isLampBroken()) {
+    lamp.src = "./img/ligada.jpg";
+  }
+}
+
+function lampOff() {
+    if (!isLampBroken()) {
+  lamp.src = "./img/desligada.jpg";
+    }
+}
+
+function lampBroken() {
+  lamp.src = "./img/quebrada.jpg";
+}
+
+turOn.addEventListener("click", lampOn);
+turOff.addEventListener("click", lampOff);
+lamp.addEventListener("mouseover", lampOn);
+lamp.addEventListener("mouseleave", lampOff);
+lamp.addEventListener("dblclick", lampBroken);
